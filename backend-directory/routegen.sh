caa74c3ec9b4882e70cbcaa429a4171159098f67
mvdhnm <mvdhnm@umsystem.edu>
2025-02-24 17:09:30 -0600

    changed routegen.sh to make directory for generated files

--- backend-directory/routegen.sh
+++ backend-directory/routegen.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # sudo apt install graphviz 
+if [ ! -d "./generatedfiles" ]; then
+  mkdir generatedfiles
+fi
 python3 aircraft_routing_problem_generator.py
 java -jar plantuml-1.2025.1.jar ./generatedfiles/full_world.puml ./generatedfiles/sparse_world.puml
 rm ./generatedfiles/full_world.puml ./generatedfiles/sparse_world.puml
