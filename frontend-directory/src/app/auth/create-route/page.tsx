a7e8ab80096efd0bbc9646439fd91f53b6bd3af8
Ayman Rahman <ayman2002rahman@gmail.com>
2025-04-25 18:07:07 -0500

    fixed card tilt bug

---
+++ frontend-directory/src/app/auth/create-route/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export default function CreateRoutePage() {
+    return (
+        <div>
+            <h1>Dashboard page</h1>
+        </div>
+    );
+};
